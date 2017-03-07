22315642ebf2e4228aea1ebb5e7607a4b542f102
Daniel Vega <danve4@gmail.com>
2017-03-06 23:55:52 -0600

    Lists the routes and the directions of the selected route

--- public/js/site.js
+++ public/js/site.js
@@ -1,12 +1,38 @@
 $.noConflict();
 jQuery(function($) {
   $(document).ready(function() {
+    var routes;
     $.getJSON('stops.json', function(json) {
-      for(var i=0; i<json.routes.length; i++) {
+      routes = json;
+      listRoutes();
+    });
+
+    function listRoutes() {
+      for(var i=0; i<routes.length; i++) {
         $('#routes').append(
-          '<li><a href="#">'+json.routes[i].rt+'</a></li>'
+          '<li><a href="#" class="route-number" id="'+i+'">'
+          +routes[i].routeNumber+ ' ' +routes[i].routeName+
+          '</a>' +
+          '</li>'
         );
       }
+    }
+    
+    $('#routes').on('click','.route-number',function (e) {
+      console.log($(this).attr('id'));
+      listRouteDirections($(this).attr('id'));
+      e.preventDefault();
     });
+
+    function listRouteDirections(rNumber) {
+      var route = routes[rNumber];
+      $('#route-directions').empty();
+      for(var j=0;j<route.directions.length;j++) {
+        $('#route-directions').append(
+          '<li>'+route.directions[j]+'</li>'
+        );
+      }
+    }
+    
   });
 });
