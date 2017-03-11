80808d2b5f0b2677fd5587f2c5a8a5f3ea300e84
Daniel Vega <danve4@gmail.com>
2017-03-11 11:28:29 -0600

    Added listing of train lines and stops, no predictions yet

--- public/js/site.js
+++ public/js/site.js
@@ -4,12 +4,18 @@ jQuery(function($) {
     var FAV='fav',ROUTES='routes',DIRECT='directions',STOPS='stops',ARRIVALS='arrivals';
     var storageItem = 'favorites'; //Name of item in localStorage
     var favorites = [];
+    var trainLines;
     var routes;
-    $.getJSON('stops.json', function(json) {
-      routes = json;
-      console.log('loaded routes');
-      decideScreen();
+    $.getJSON('train-stops-endpoints.json', function(json) {
+      trainLines = json;
+      console.log('loaded trainLines');
+      $.getJSON('stops.json', function(json) {
+        routes = json;
+        console.log('loaded routes');
+        decideScreen();
+      });
     });
+
 
     function decideScreen() {
       if(!location.hash) {
@@ -22,29 +28,51 @@ jQuery(function($) {
           listFavorites();
         } else  if(context.hasOwnProperty('routes')) {
           setScreenTo(ROUTES);
+          listTrainLines();
           listRoutes();
-        } else if(context.hasOwnProperty('rt') &&
-                  !context.hasOwnProperty('dir')) {
-          setScreenTo(DIRECT);
-          listRouteDirections(context.rt);
-        } else if(context.hasOwnProperty('rt') &&
-                  context.hasOwnProperty('dir') &&
+        } else if(context.hasOwnProperty('rt')) {
+          if(!context.hasOwnProperty('dir')) {
+            setScreenTo(DIRECT);
+            listRouteDirections(context.rt);
+          }
+          else if(context.hasOwnProperty('dir') &&
                   !context.hasOwnProperty('stop-id')) {
-          setScreenTo(STOPS);
-          getRouteStops(context.rt,context['dir']);
-        } else if(context.hasOwnProperty('rt') &&
-                  context.hasOwnProperty('rt-name') &&
+            setScreenTo(STOPS);
+            getRouteStops(context.rt,context['dir']);
+          }
+          else if(context.hasOwnProperty('rt-name') &&
                   context.hasOwnProperty('dir') &&
                   context.hasOwnProperty('stop-id')) {
-          setScreenTo(ARRIVALS);
-          getPredictions(context['rt-name'].replace(/%20/g, ' '),context['dir'],context['stop-id']);
-          checkFavorite();
+            setScreenTo(ARRIVALS);
+            getBusPredictions(context['rt-name'].replace(/%20/g, ' '),context['dir'],context['stop-id']);
+            checkFavorite();
+          }
+        } else if(context.hasOwnProperty('tl')) {
+          if(!context.hasOwnProperty('dir')) {
+            setScreenTo(DIRECT);
+            listLineDirections(context['tl'])
+          } else if(context.hasOwnProperty('dir')) {
+            setScreenTo(STOPS);
+            listLineStops(context['tl'], context['dir']);
+          }
         }
       }
     }
 
-    function listRoutes() {
+    function listTrainLines() {
       $('#routes').empty();
+      var line;
+      for(var i=0; i<trainLines.trainLines.length; i++) {
+        line = trainLines.trainLines[i];
+        $('#routes').append(
+          '<li>' +
+            '<a href="#tl='+i+'">'+line.lineName+'</a>'+
+          '</li>'
+        );
+      }
+    }
+
+    function listRoutes() {
       var route;
       for(var i=0; i<routes.routes.length; i++) {
         route = routes.routes[i];
@@ -59,6 +87,19 @@ jQuery(function($) {
       }
     }
 
+    function listLineDirections(lineIndex) {
+      var line = trainLines.trainLines[lineIndex];
+      $('#route-directions').empty();
+      $('#route-directions').append('<li class="list-subheader">'+line.lineName+' - Choose a direction</li>');
+      for(var i=0;i<line.directions.length;i++) {
+        $('#route-directions').append(
+          '<li><a href="#tl='+lineIndex+'#dir='+i+'">'
+          +line.directions[i].direction+
+          '</a></li>'
+        );
+      }
+    }
+
     function listRouteDirections(rNumber) {
       var route = routes[rNumber];
       $('#route-directions').empty();
@@ -67,6 +108,22 @@ jQuery(function($) {
         $('#route-directions').append(
           '<li><a href="#rt='+rNumber+'#dir='+route.directions[j]+'">'
           +route.directions[j]+
+          '</a></li>'
+        );
+      }
+    }
+
+    function listLineStops(lineIndex, directionIndex) {
+      var line = trainLines.trainLines[lineIndex];
+      var direction = line.directions[directionIndex];
+      $('#stops').empty();
+      $('#stops').append(
+        '<li class="list-subheader">'+line.lineName+' - '+ direction.direction+' -  Choose a stop</li>'
+      );
+      for(var i=0;i<direction.stops.length;i++) {
+        $('#stops').append(
+          '<li><a href="#tl='+lineIndex+'#dir='+directionIndex+'#stop='+i+'">'
+          +direction.stops[i].stationName+
           '</a></li>'
         );
       }
@@ -97,7 +154,11 @@ jQuery(function($) {
       }
     }
 
-    function getPredictions(routeName, direction, stopId) {
+    function getTrainPredictions(lineIndex, directionIndex, stopIndex) {
+
+    }
+
+    function getBusPredictions(routeName, direction, stopId) {
       $('#arrivals').empty();
       $('#arrivals').append('<li class="list-subheader">'+routeName+' - '+ direction+'</li>');
       $.when($.ajax({
